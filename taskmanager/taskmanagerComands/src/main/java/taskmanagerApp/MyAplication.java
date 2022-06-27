dc3e1198f5a254429a05ad36fe60310f13108a99
cirligelena <cirlig.elena@mail.ru>
2022-06-27 15:29:12 +0300

    javadoc descriptions for packages added

--- taskmanager/taskmanagerComands/src/main/java/taskmanagerApp/MyAplication.java
+++ taskmanager/taskmanagerComands/src/main/java/taskmanagerApp/MyAplication.java
@@ -1,6 +1,12 @@
 package taskmanagerApp;
 
 import commands.CommandFactory;
+/**
+ * The main class of the 
+* Starts the program using main method String[] args arguments 
+ * @author ecirlig
+ *
+ */
 
 public class MyAplication {
 	static CommandFactory commandFactory = new CommandFactory();
---
+++ taskmanager/taskmanagerDAO/src/main/java/helper/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * An auxiliary package used to get environment variables values from files
+ */
+package helper;
